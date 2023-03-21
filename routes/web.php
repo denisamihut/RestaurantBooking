e76cf97b758ea13bf108fd5a09fa24cbc886f5fc
denisamihut <denisa.m_19@yahoo.com>
2023-03-21 11:25:09 +0200

    added home controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use app\Http\Controller\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index']);
